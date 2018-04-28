2f7eb83629bfc3a19ba7daece924594594b3b02b
Unknown <feladorn@gmail.com>
2018-04-28 00:50:24 -0400

    Boss bullet

---
+++ Assets/scripts/Dropper.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Dropper : MonoBehaviour {
+
+	public float frequency;
+	public GameObject item;
+
+	private Transform me;
+
+	// Use this for initialization
+	void Start () {
+		me = GetComponent<Transform>();
+		InvokeRepeating ("DropItem", frequency, frequency);
+	}
+
+	void DropItem () {
+		Instantiate (item, me.position, Quaternion.identity);
+	}
+
+
+
+}
