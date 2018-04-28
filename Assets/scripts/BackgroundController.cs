eeac7572a0bbff3c123cd9a200809855ff343883
MGR <maikgibranr@gmail.com>
2018-04-28 15:32:17 +0700

    Add scrolling background; Modify ships setting;

---
+++ Assets/scripts/BackgroundController.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BackgroundController : MonoBehaviour {
+
+    public float scrollSpeed;
+    public float tileSizeZ;
+
+    private Vector3 initialPos;
+
+	void Start () {
+        initialPos = transform.position;
+	}
+	
+	void Update () {
+        float newPos = Mathf.Repeat(Time.time * scrollSpeed, tileSizeZ);
+        transform.position = initialPos + Vector3.left * newPos;
+	}
+}
